32f9f90506d1458cb792a42b1d482eaebddfff3c
Sean Watson <sean.watson@shopify.com>
2021-08-18 09:54:13 -0400

    Adding nice plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,15 @@
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+sh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~"
+sh -c " git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
 
 # ZSH
 ln -sfn ~/dotfiles/zsh ~/.zsh
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 
+source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zshrc
 
 # Ensure that worker logs will scroll and you can use binding.pry for Shopify
